33cf7c1223c254bdbe55d0cef04eed5a8ba255da
Sócrates Freire Vasconcelos <115791477+socratesvasconcelos@users.noreply.github.com>
2023-02-28 21:07:44 -0300

    Transformador de milhas para km com python

---
+++ milhas_para_km.py
@@ -0,0 +1,7 @@
+print("TRANSFORMADOR DE MILHAS EM KM")
+milhas_value= float(input("Digite a qtd de milhas desejadas para transformar em KM : "))
+
+valorr_fx = 1.6
+transf = valorr_fx * milhas_value
+
+print(f"o valor {milhas_value} MPH transformado em KM é de {transf} km")
